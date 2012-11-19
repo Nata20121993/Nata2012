0212e1aca73d8cfefd5a2fdeb7fb5cb511ec1115
Nata20121993 <nata20121993@gmail.com>
2012-11-19 09:17:59 +0200

    commit

---
+++ lab11/лаба11/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace лаба11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstBx1 = new System.Windows.Forms.ListBox();
+            this.lstBx2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(440, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Найти количество элементов массива A={a[i]} меньших среднего арифметического. \r\n\r" +
+                "\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(435, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Удалить из исходного вектора все элементы меньшие среднего арифметического.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Размер вектора:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(31, 111);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(103, 48);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Формирование вектора";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(143, 111);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(97, 48);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Получение результата";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Количество элементов массива";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 241);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(311, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Исходный вектор ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(437, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Результирующий вектор";
+            // 
+            // lstBx1
+            // 
+            this.lstBx1.Location = new System.Drawing.Point(305, 92);
+            this.lstBx1.Name = "lstBx1";
+            this.lstBx1.Size = new System.Drawing.Size(105, 251);
+            this.lstBx1.TabIndex = 10;
+            // 
+            // lstBx2
+            // 
+            this.lstBx2.FormattingEnabled = true;
+            this.lstBx2.Location = new System.Drawing.Point(440, 92);
+            this.lstBx2.Name = "lstBx2";
+            this.lstBx2.Size = new System.Drawing.Size(120, 251);
+            this.lstBx2.TabIndex = 11;
+            this.lstBx2.SelectedIndexChanged += new System.EventHandler(this.lstBx2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(64, 306);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 384);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lstBx2);
+            this.Controls.Add(this.lstBx1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Наталия Дубовик";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lstBx1;
+        private System.Windows.Forms.ListBox lstBx2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab11/лаба11/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace лаба11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] vector;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ushort size = ushort.Parse(textBox1.Text);
+                vector = new int[size];
+                form_vector(ref vector);//Формирование вектора
+                output_vector(vector, lstBx1);
+                btn2.Visible = true;
+                btn2.Focus();//Вывод вектрона в лист-бокс с исходным вектором
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вводите только цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        static void form_vector(ref int[] A)//Функци формирующая вектор
+        {
+            Random m = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                //do
+                //{
+                A[i] = m.Next(A.Length) - m.Next(A.Length);
+                //}
+                // while (A[i] == 0);
+            }
+        }
+        static void output_vector(int[] A, ListBox lsbx)//Функция выводящая векторв  лист-бокс с исходным вектором
+        {
+            lsbx.Items.Clear();
+            foreach (int el in A)
+                lsbx.Items.Add(el);
+        }
+
+
+        private void lstBx2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            int proiz = 1;
+            proiz = calc(vector, proiz);//Вычисление среднего арифметического
+            //Del_elem_vector(ref vector);//Удаление элементов меньше среднего арифметического
+            output_vector(vector, lstBx2);//Вывод элементов векторы в лист-бокс
+            textBox2.Text = proiz.ToString();
+        }
+            //textBox2.Visible = true;
+            //textBox2.Enabled = false;
+            //btn2.Visible = false;
+        static void RandArray(int[] Mas)
+        {
+            Random m = new Random();//инициализация датчика случайных чисел
+            for (int i = 0; i < Mas.Length; i++)
+                Mas[i] = m.Next(Mas.Length);//получение случайного числа в диапазоне от 0 до длины вектора
+        }
+            static int calc(int [] vector, int proiz)//Фунция вычисляющая среднее арифметическое
+            {
+                //Random m = new Random();//инициализация датчика случайных чисел
+                //for (int i = 0; i < Mas.Length; i++)
+                //    Mas[i] = m.Next(Mas.Length);//получение случайного числа в диапазоне от 0 до длины вектора
+
+                      double sum = 0;
+            foreach (int el in Mas)
+                sum += el;
+            return sum / Mas.Length; 
+                  }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    //        Console.Write("Input size vectora-->");
+    //        int size = int.Parse(Console.ReadLine());
+    //        int[] a = new int[size]; //создали вектор размера size;
+    //        RandArray(a);
+    //        Console.WriteLine("Initial vector:");
+    //        PrintArray(a);
+    //        Console.Write("Input new size vectora-->");
+    //        int newsize = int.Parse(Console.ReadLine());
+    //        Array.Resize(ref a, newsize);
+    //        Console.WriteLine("Change kol-el vectora:");
+    //        PrintArray(a);
+    //        Array.Sort(a);
+    //        Console.WriteLine("Sorting vector:");
+    //        PrintArray(a);
+    //        Console.WriteLine("Rank vectora=" + a.Rank.ToString());
+    //        Console.WriteLine("Length vectora=" + a.Length.ToString());
+    //        Console.WriteLine("Average el-tov vectora=" + AverageArray(a).ToString());
+
+    //        }
+    //           static void RandArray(int[] Mas)
+    //    {
+    //        Random m = new Random();//инициализация датчика случайных чисел
+    //        for (int i = 0; i < Mas.Length; i++)
+    //            Mas[i] = m.Next(Mas.Length);//получение случайного числа в диапазоне от 0 до длины вектора
+    //    }
+    //    static void PrintArray(int[] Mas)
+    //    {
+    //        foreach (int el in Mas)
+    //            Console.Write(el.ToString()+"\t");
+    //        Console.WriteLine();
+    //    }
+    //    static double AverageArray(int[] Mas)
+    //    {
+    //        double sum = 0;
+    //        foreach (int el in Mas)
+    //            sum += el;
+    //        return sum / Mas.Length;
+    //    }
+
+    }
+        }
+    
